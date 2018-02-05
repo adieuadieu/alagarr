f6a0a35ff14571a1f5d5fbbbe11fedb09f7f9223
Marco Lüthy <marco@luethy.net>
2018-02-05 16:50:07 +0100

    small improvement to error handler messages

--- src/index.ts
+++ src/index.ts
@@ -18,32 +18,36 @@ const defaultErrorHandler = (
 
   // throw any https://github.com/jshttp/http-errors
 
-  if (
-    error.name === 'ClientError' ||
-    (error.statusCode >= 400 && error.statusCode < 500)
-  ) {
-    return response.respondTo(
-      {
-        html: `<html><body>${
-          error.message
-        }<br/>Request ID: ${requestId}</body></html>`,
-        json: { error: error.name, message: error.message, requestId },
-      },
-      error.statusCode || 400,
-    )
-  }
+  const errorName =
+    error.name && typeof error.name === 'string'
+      ? error.name.replace(
+          /([A-Z])([A-Z])([a-z])|([a-z])([A-Z])/g,
+          '$1$4 $2$3$5',
+        )
+      : 'Error'
+  const errorMessage = error.message || ''
+  const statusCode = error.statusCode ? ` ${error.statusCode}` : ''
 
-  return response.respondTo(
-    {
-      html: `<html><body>An internal server error occurred.<br/>Request ID: ${requestId}</body></html>`,
-      json: {
-        error: 'Internal server error',
-        message: 'An internal server error occurred',
-        requestId,
-      },
-    },
-    500,
-  )
+  return error.name === 'ClientError' ||
+    (error.statusCode >= 400 && error.statusCode < 500)
+    ? response.respondTo(
+        {
+          html: `<html><body><strong>${errorName}${statusCode}</strong>:${errorMessage}<br/>Request ID: ${requestId}</body></html>`,
+          json: { error: error.name, message: errorMessage, requestId },
+        },
+        error.statusCode || 400,
+      )
+    : response.respondTo(
+        {
+          html: `<html><body>An internal server error occurred.<br/>Request ID: ${requestId}</body></html>`,
+          json: {
+            error: 'Internal server error',
+            message: 'An internal server error occurred',
+            requestId,
+          },
+        },
+        500,
+      )
 }
 
 const DEFAULT_OPTIONS = {
