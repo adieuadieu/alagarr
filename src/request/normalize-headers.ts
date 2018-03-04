24e1071a0a08de606a877bc047c825be05517d1c
Marco Lüthy <marco@luethy.net>
2018-03-04 20:12:35 +0100

    refactor: clean up request headers-normalizing middleware

--- src/request/normalize-headers.ts
+++ src/request/normalize-headers.ts
@@ -13,21 +13,21 @@ export default function normalizeHeaders({
       /*
         Lame workaround for https://github.com/dherault/serverless-offline/pull/295
         https://github.com/dherault/serverless-offline/blob/97dc5eab4deb9af673a1b66f3f7a7367f82bf727/src/createLambdaProxyContext.js#L25-L27
-        If the request contains header 'content-type', serverless-offline will interpret this as "there is no content type header" and adds a "Content-Type" header to the simulated aws-gateway request. This then causes problems when we normalize the headers as the "Content-Type" header will overwrite "content-type" header from the real request when we "Content-Type".toLowerCase()
+        If the request contains header 'content-type', serverless-offline will interpret
+        this as "there is no content type header" and adds a "Content-Type" header to
+        the simulated aws-gateway request. This then causes problems when we normalize
+        the headers as the "Content-Type" header will overwrite "content-type" header
+        from the real request when we "Content-Type".toLowerCase()
       */
-      if (
-        request.provider === 'aws' &&
+      return request.provider === 'aws' &&
         Reflect.has(normalizedHeaders, normalizedHeaderKey) &&
         normalizedHeaderKey === 'content-type' &&
         headers[key] === 'application/json'
-      ) {
-        return normalizedHeaders
-      }
-
-      return {
-        ...normalizedHeaders,
-        [normalizedHeaderKey]: headers[key],
-      }
+        ? normalizedHeaders
+        : {
+            ...normalizedHeaders,
+            [normalizedHeaderKey]: headers[key],
+          }
     }, {}),
   }
 }
