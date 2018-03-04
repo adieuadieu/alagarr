4fce81deea77566368946fcdaafe9e6fc2190098
Marco Lüthy <marco@luethy.net>
2018-03-04 20:01:25 +0100

    refactor: clean up request base64 body decoding middleware

--- src/request/decode.ts
+++ src/request/decode.ts
@@ -1,15 +1,19 @@
 import { InterfaceRequest } from '../types'
 
+const decodeBase64 = (encoded: string): string => {
+  const bodyBuffer = Buffer.from(encoded, 'base64')
+
+  return bodyBuffer.toString('utf8')
+}
+
 // Decodes the request body if it's been base64 encoded by API Gateway
-export default function decodeBase64Body(request: InterfaceRequest): InterfaceRequest {
-  if (request.isBase64Encoded && typeof request.body === 'string') {
-    const bodyBuffer = Buffer.from(request.body, 'base64')
-
-    return {
-      ...request,
-      body: bodyBuffer.toString('utf8'),
-    }
-  }
-
-  return request
+export default function decodeBase64Body(
+  request: InterfaceRequest,
+): InterfaceRequest {
+  return request.isBase64Encoded && typeof request.body === 'string'
+    ? {
+        ...request,
+        body: decodeBase64(request.body),
+      }
+    : request
 }
