43a3a8c140cbf952bc64a421b11f4203afaa82e6
Marco Lüthy <marco@luethy.net>
2018-03-04 20:06:55 +0100

    refactor: clean up request json body decoding middleware

--- src/request/jsonBody.ts
+++ src/request/jsonBody.ts
@@ -1,24 +1,23 @@
 import { InterfaceRequest } from '../types'
 // import { ClientError } from '../utils/errors'
 
+const parseJson = (stringified: string): any => {
+  try {
+    return JSON.parse(stringified)
+  } catch {
+    // throw ClientError('Invalid JSON payload in request-body.')
+    return {}
+  }
+}
+
 // Parses JSON request body, if there is one
 export default function parseJsonBody(
   request: InterfaceRequest,
 ): InterfaceRequest {
   const { headers, body } = request
 
-  if (
-    headers['content-type'] === 'application/json' &&
+  return headers['content-type'] === 'application/json' &&
     typeof body === 'string'
-  ) {
-    try {
-      const json = JSON.parse(body)
-      return { ...request, body: json }
-    } catch (error) {
-      // throw ClientError('Invalid JSON payload in request-body.')
-      return { ...request, body: {} }
-    }
-  }
-
-  return request
+    ? { ...request, body: parseJson(body) }
+    : request
 }
