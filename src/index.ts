21fb2d3ec45a81fbce91ed99a4914ac1f22bcbbb
Marco Lüthy <marco@luethy.net>
2018-03-18 18:04:01 +0100

    refactor: move default error handler into it's own file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import defaultErrorHandler from './error-handler'
 import ClientError from './errors/client-error'
 import ServerError from './errors/server-error'
 import parseRequest from './request'
@@ -9,47 +10,6 @@ import {
   InterfaceRequest,
   InterfaceResponse,
 } from './types'
-
-const defaultErrorHandler = (
-  request: InterfaceRequest,
-  response: InterfaceResponse,
-  error: any,
-) => {
-  const { requestId } = request.requestContext
-
-  // throw any https://github.com/jshttp/http-errors
-
-  const errorName =
-    error.name && typeof error.name === 'string'
-      ? error.name.replace(
-          /([A-Z])([A-Z])([a-z])|([a-z])([A-Z])/g,
-          '$1$4 $2$3$5',
-        )
-      : 'Error'
-  const errorMessage = error.message || ''
-  const statusCode = error.statusCode ? ` ${error.statusCode}` : ''
-
-  return error.name === 'ClientError' ||
-    (error.statusCode >= 400 && error.statusCode < 500)
-    ? response.respondTo(
-        {
-          html: `<html><body><strong>${errorName}${statusCode}</strong>: ${errorMessage}<br/>Request ID: ${requestId}</body></html>`,
-          json: { error: error.name, message: errorMessage, requestId },
-        },
-        error.statusCode || 400,
-      )
-    : response.respondTo(
-        {
-          html: `<html><body>An internal server error occurred.<br/>Request ID: ${requestId}</body></html>`,
-          json: {
-            error: 'Internal server error',
-            message: 'An internal server error occurred',
-            requestId,
-          },
-        },
-        500,
-      )
-}
 
 const DEFAULT_OPTIONS: InterfaceAlagarrOptions = {
   cspPolicies: [],
@@ -67,7 +27,7 @@ const DEFAULT_OPTIONS: InterfaceAlagarrOptions = {
 
 const noopHandler = (_: void, _1: InterfaceResponse) => {
   throw new ServerError(
-    'Misconfiguration in Alagarr setup. Failed to provide a handler function.',
+    'Misconfiguration in Alagarr setup. No handler function was provided.',
   )
 }
 
