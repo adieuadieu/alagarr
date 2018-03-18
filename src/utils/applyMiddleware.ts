d4ebf93501c2308a35260825985f6a19dd69dac1
Marco Lüthy <marco@luethy.net>
2018-03-18 16:30:09 +0100

    refactor: fix generic typedefs on applyMiddleware

--- src/utils/applyMiddleware.ts
+++ src/utils/applyMiddleware.ts
@@ -1,19 +1,14 @@
-import {
-  InterfaceRequest,
-  InterfaceResponse,
-  RequestMiddleware,
-  ResponseMiddleware,
-} from '../types'
-
 // aka.... compose.. or rather, pipe?
+export type Middleware<T> = (...args: any[]) => T // tslint:disable-line readonly-array
 
 export default async function applyMiddleware<T>(
-  middlewareList: ReadonlyArray<RequestMiddleware | ResponseMiddleware>,
-  initialData: any,
-  ...args: any[] //ReadonlyArray<TT>[]
+  middlewareList: ReadonlyArray<Middleware<T>>,
+  initialData: T,
+  ...args: any[] // tslint:disable-line readonly-array
 ): Promise<T> {
   return middlewareList.reduce(
-    async (applied: T, middleware: any) => middleware(await applied, ...args),
-    initialData,
+    async (applied: Promise<T>, middleware: Middleware<T>) =>
+      middleware(await applied, ...args),
+    Promise.resolve(initialData),
   )
 }
