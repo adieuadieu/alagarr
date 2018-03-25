c064975b4f59d7f0ed93aa0405317875659174b8
Marco Lüthy <marco@luethy.net>
2018-03-25 22:46:24 +0200

    refactor: logger util into log response middleware

--- src/response/middleware/log.test.ts
+++ src/response/middleware/log.test.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-expression-statement
 import { get as getRequestFixture } from '../../../test/fixtures/requests'
-import log from './log'
+import log, { logger } from './log'
 
 const mockRequest = {
   ...getRequestFixture,
@@ -20,19 +20,14 @@ const mockOptions = {
 
 describe('Request/Response logging ', () => {
   test('is logged by default logger when none is provided in options', () => {
-    const mockLogger = jest.fn()
+    const spy = jest
+      .spyOn(process.stdout, 'write')
+      .mockImplementationOnce(item => expect(item).toContain('statusCode'))
 
-    jest.doMock('../../utils/logger', () => ({ default: mockLogger }))
-    jest.resetModules()
+    const result = log(mockResponse, mockRequest, mockOptions)
 
-    const response = require('./log').default(
-      mockResponse,
-      mockRequest,
-      mockOptions,
-    )
-
-    expect(mockLogger).toHaveBeenCalledTimes(1)
-    expect(response.statusCode).toBe(200)
+    expect(result.statusCode).toBe(200)
+    expect(spy).toHaveBeenCalledTimes(1)
   })
 
   test('is logged by custom options.logger if one is provided', () => {
@@ -69,5 +64,81 @@ describe('Request/Response logging ', () => {
 
     expect(mockCustomLogger).toHaveBeenCalledTimes(0)
     expect(response).toBe(response)
+  })
+})
+
+describe('Default logger', () => {
+  test('should construct a requestUri when request contained query string parameters', () => {
+    jest.spyOn(process.stdout, 'write').mockImplementationOnce((_: any) => true)
+    expect(logger(mockRequest, mockResponse).requestUri).toBe('/')
+
+    jest.spyOn(process.stdout, 'write').mockImplementationOnce((_: any) => true)
+    expect(
+      logger(
+        { ...mockRequest, queryStringParameters: { foo: 'bar', bar: 'foo' } },
+        mockResponse,
+      ).requestUri,
+    ).toBe('/?foo=bar&bar=foo')
+  })
+
+  test('should write to process.stdout when statusCode is not in the 500s', () => {
+    jest
+      .spyOn(process.stdout, 'write')
+      .mockImplementationOnce(item => !expect(item).toContain('statusCode'))
+
+    const result = logger(mockRequest, mockResponse)
+
+    expect(result.statusCode).toBe(200)
+  })
+
+  test('should write to process.stderr when statusCode is in the 500s', () => {
+    const spy = jest
+      .spyOn(process.stderr, 'write')
+      .mockImplementationOnce(item => !expect(item).toContain('statusCode'))
+
+    const result = logger(mockRequest, { ...mockResponse, statusCode: 500 })
+
+    expect(result.statusCode).toBe(500)
+    expect(spy).toHaveBeenCalledTimes(1)
+  })
+
+  test('should include httpProtocol', () => {
+    jest.spyOn(process.stdout, 'write').mockImplementationOnce((_: any) => true)
+
+    const result = logger(
+      {
+        ...mockRequest,
+        headers: { ...mockRequest.headers, via: 'foo bar' },
+      },
+      mockResponse,
+    )
+
+    expect(result.httpProtocol).toBe('foo')
+  })
+
+  test('should include request content-length', () => {
+    jest.spyOn(process.stdout, 'write').mockImplementationOnce((_: any) => true)
+
+    expect(
+      logger(
+        {
+          ...mockRequest,
+          body: 'foobar',
+        },
+        mockResponse,
+      ).requestContentLength,
+    ).toBe(6)
+
+    jest.spyOn(process.stdout, 'write').mockImplementationOnce((_: any) => true)
+
+    expect(
+      logger(
+        {
+          ...mockRequest,
+          body: undefined,
+        },
+        mockResponse,
+      ).requestContentLength,
+    ).toBe(0)
   })
 })
--- src/response/middleware/log.ts
+++ src/response/middleware/log.ts
@@ -1,9 +1,88 @@
+import * as querystring from 'querystring'
 import {
   InterfaceAlagarrOptions,
   InterfaceRequest,
   InterfaceResponseData,
 } from '../../types'
-import logger from '../../utils/logger'
+
+function aws(request: InterfaceRequest): object {
+  const { headers: requestHeaders, requestContext, context } = request
+
+  return {
+    awsAccountId: requestContext.accountId,
+    deploymentStage: requestContext.stage,
+
+    // API Gateway stuff
+    apig: {
+      requestId: requestContext.requestId,
+      resourcePath: requestContext.resourcePath,
+    },
+
+    // cloudfront
+    cloudfront: {
+      country: requestHeaders['cloudfront-viewer-country'],
+      desktop: requestHeaders['cloudfront-is-desktop-viewer'] === 'true',
+      mobile: requestHeaders['cloudfront-is-mobile-viewer'] === 'true',
+      smarttv: requestHeaders['cloudfront-is-smarttv-viewer'] === 'true',
+      tablet: requestHeaders['cloudfront-is-tablet-viewer'] === 'true',
+    },
+
+    lambda: {
+      functionName: context.functionName,
+      memoryLimit: Number(context.memoryLimitInMB),
+      remainingExecutionTime:
+        context.getRemainingTimeInMillis && context.getRemainingTimeInMillis(),
+      requestId: context.awsRequestId,
+    },
+
+    sourceIp: requestContext.identity.sourceIp,
+  }
+}
+
+export function logger(
+  request: InterfaceRequest,
+  response: InterfaceResponseData,
+): any {
+  const {
+    source,
+    headers: requestHeaders,
+    meta,
+    requestContext,
+    provider,
+  } = request
+  const error = response.statusCode >= 500 && response.statusCode < 600
+
+  const logEntry = {
+    dateTime: new Date().toISOString(),
+    eventSource: source,
+    hostname: request.hostname,
+    httpMethod: requestContext.httpMethod,
+    httpProtocol: requestHeaders.via && requestHeaders.via.split(' ')[0],
+    httpReferrer: requestHeaders.referer,
+    httpUserAgent: requestHeaders['user-agent'],
+    requestContentLength: (request.body && request.body.length) || 0,
+    requestTime: Date.now() - request.timestamp,
+    requestUri:
+      request.path +
+      (request.queryStringParameters &&
+      Object.keys(request.queryStringParameters).length
+        ? `?${querystring.stringify(request.queryStringParameters)}`
+        : ''),
+    responseContentLength: response.headers['content-length'],
+    statusCode: response.statusCode,
+    xForwardedHost: requestHeaders['x-forwarded-host'],
+
+    ...meta,
+
+    // Provider specific properties
+    ...(provider === 'aws' && aws(request)),
+  }
+
+  return (
+    (error ? process.stderr : process.stdout).write(JSON.stringify(logEntry)) &&
+    logEntry
+  )
+}
 
 // Logs request/response info
 export default function logResponse(
