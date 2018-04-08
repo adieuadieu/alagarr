4392d083fbd112ef46070f7d4df2c25b957e6e1c
Marco Lüthy <marco@luethy.net>
2018-04-08 19:12:44 +0200

    feat: support passing an already stringified json string to response.json()

--- src/response/json.test.ts
+++ src/response/json.test.ts
@@ -1,5 +1,6 @@
 // tslint:disable:no-expression-statement
 import makeResponse from './'
+import json from './json'
 
 const testRequest = { headers: {} } as any
 const testJsonBody = { foo: 'bar' }
@@ -19,4 +20,11 @@ it('Content-Type is JSON', async () => {
   )
 
   response.json(testJsonBody, testStatusCode)
+})
+
+it('should not stringify when body is already stringified JSON', async () => {
+  expect(json({} as any, 'foobar').body).toEqual('"foobar"')
+  expect(json({} as any, JSON.stringify(testJsonBody)).body).toEqual(
+    JSON.stringify(testJsonBody),
+  )
 })
--- src/response/json.ts
+++ src/response/json.ts
@@ -1,6 +1,17 @@
 import { InterfaceResponseData, ResponseHelper } from '../types'
 import makeResponseObject from './make-response-object'
 
+// Somewhat lazy implementation, but cheaper than verifying the string with
+// JSON.parse() to see if it's a JSON string.
+function stringifyIfNotStringifiedJson(data: any): string {
+  const isStringifiedJson =
+    typeof data === 'string' &&
+    ['{', '"'].includes(data[0]) &&
+    ['}', '"'].includes(data[data.length - 1])
+
+  return isStringifiedJson ? data : JSON.stringify(data)
+}
+
 const json: ResponseHelper = (
   _,
   body,
@@ -8,7 +19,7 @@ const json: ResponseHelper = (
   options,
 ): InterfaceResponseData =>
   makeResponseObject(
-    JSON.stringify(body),
+    stringifyIfNotStringifiedJson(body),
     statusCode,
     options,
     'application/json',
